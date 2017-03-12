8b65663589c5f5bad8329e143dc74e3cca0a13bd
Chris Seieroe <chris@seieroe.com>
2017-03-12 13:37:05 -0700

    add stat-block

---
+++ src/app/stat-block/stat-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'stat-block',
+  templateUrl: './stat-block.component.html',
+  styleUrls: ['./stat-block.component.css']
+})
+export class StatBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

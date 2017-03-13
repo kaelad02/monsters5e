ebbe6fc153e74862668d983d7a492f3ee28f65ea
Chris Seieroe <chris@seieroe.com>
2017-03-12 17:54:16 -0700

    add abilities-block component
    commented out tapered-rule, need to uncomment once that component is
    added

---
+++ src/app/abilities-block/abilities-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+@Component({
+  selector: 'abilities-block',
+  templateUrl: './abilities-block.component.html',
+  styleUrls: ['./abilities-block.component.css']
+})
+export class AbilitiesBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { StatBlockComponent } from './stat-block/stat-block.component';
 import { CreatureHeadingComponent } from './creature-heading/creature-heading.component';
 import { PropertyLineComponent } from './property-line/property-line.component';
+import { AbilitiesBlockComponent } from './abilities-block/abilities-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StatBlockComponent,
     CreatureHeadingComponent,
-    PropertyLineComponent
+    PropertyLineComponent,
+    AbilitiesBlockComponent
   ],
   imports: [
     BrowserModule,

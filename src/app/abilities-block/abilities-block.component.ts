6d66ad6d0eb72966d57ee75c1602b3d8d542dc82
Chris Seieroe <chris@seieroe.com>
2017-03-19 03:50:53 -0700

    set ability scores using properties

--- src/app/abilities-block/abilities-block.component.ts
+++ src/app/abilities-block/abilities-block.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
+import { AbilityScores, Ability } from '../monster';
 
 @Component({
   selector: 'abilities-block',
@@ -6,10 +7,40 @@ import { Component, OnInit, Inject } from '@angular/core';
   styleUrls: ['./abilities-block.component.css']
 })
 export class AbilitiesBlockComponent implements OnInit {
+  @Input() abilityScores: AbilityScores;
 
-  constructor() { }
+  private readonly abilities: number[];
+  private readonly labels: string[];
+
+  constructor() {
+    /*
+    * Enums are transpiled into tuples of their number and string values in both
+    * directions. For example:
+    * [0, "first"], [1, "second"], ["first", 0], ["second", 1]
+    * The first transformation pulls out the first part of the tuple. The second
+    * keeps the first half (the numbers). Finally, it's transformed into a
+    * number array.
+    */
+    let abilities = Object.keys(Ability);
+    abilities = abilities.slice(0, abilities.length / 2);
+    this.abilities = abilities.map(function(n){
+      return Number(n);
+    });
+
+    abilities = Object.keys(Ability);
+    this.labels = abilities.slice(abilities.length / 2);
+  }
 
   ngOnInit() {
+  }
+
+  getModifier(ability: Ability): string {
+    let modifier = this.abilityScores.getModifier(ability);
+    if(modifier >= 0) {
+      return "+" + modifier;
+    }
+    // use an en dash, not a normal dash to make it more visible
+    return "–" + Math.abs(modifier);
   }
 
 }

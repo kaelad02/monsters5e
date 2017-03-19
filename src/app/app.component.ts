2a50b23ca21ef58b2cae192e19cf9cf7baf9c6f7
Chris Seieroe <chris@seieroe.com>
2017-03-18 23:57:28 -0700

    inject MonsterService to App and show monster selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MonsterService } from './monster.service';
+import { Monster } from './monster';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ MonsterService ]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  names: string[];
+  selectedName: string;
+  selectedMonster: Monster;
+
+  constructor(private monsterService: MonsterService) { }
+
+  ngOnInit(): void {
+    this.getMonsterNames();
+  }
+
+  getMonsterNames(): void {
+    this.names = this.monsterService.getNames();
+  }
+
+  onSelect(): void {
+    selectedMonster => this.monsterService.getMonster(this.selectedName);
+  }
 }

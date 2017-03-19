1212c967bb0c2092bc3ceda751a20727a10ab92a
Chris Seieroe <chris@seieroe.com>
2017-03-19 02:36:08 -0700

    set AC, HP, and speed using properties

--- src/app/top-stats/top-stats.component.ts
+++ src/app/top-stats/top-stats.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { PropertyLineComponent } from '../property-line/property-line.component';
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { AbilityScores, Ability } from '../monster';
 
 @Component({
   selector: 'top-stats',
   templateUrl: './top-stats.component.html',
   styleUrls: ['./top-stats.component.css']
 })
-export class TopStatsComponent implements OnInit {
+export class TopStatsComponent implements OnInit, OnChanges {
+  @Input() armor_class: number;
+  @Input() hit_points: number;
+  @Input() hit_dice: string;
+  hp_mod: number;
+  @Input() speed: string;
 
-  constructor(/*private props: PropertyLineComponent*/) { }
+  @Input() abilityScores: AbilityScores;
+
+  constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // TODO check if hit_dice or abilityScores changed
+
+    let index = this.hit_dice.indexOf('d');
+    let numDice = this.hit_dice.substring(0, index);
+    this.hp_mod = Number(numDice) * this.abilityScores.getModifier(Ability.CON);
   }
 
 }

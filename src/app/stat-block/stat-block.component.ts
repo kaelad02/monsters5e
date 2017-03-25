d865b0598a3d5687fc733591d28ec51b8188c388
Chris Seieroe <chris@seieroe.com>
2017-03-24 18:47:50 -0700

    clear unused imports from stat-block

--- src/app/stat-block/stat-block.component.ts
+++ src/app/stat-block/stat-block.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Monster, AbilityScores, SavingThrows } from '../monster';
+import { Monster } from '../monster';
 
 @Component({
   selector: 'stat-block',
